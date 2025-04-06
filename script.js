746be0cba80c1f41fdff1db84ad7687fc85357c3
DC <curtis.deshawn@ymail.com>
2025-04-06 07:17:05 +0000

    my understanding of document.querySelector so far is that it chooses an id or class from my html and when it is method called in javascript I am then amble to display it to the consle and make that html text interactive in a browser.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 export let titleElement = document.getElementById("title");
 
  console.log(titleElement);
+
+
+ //<h2 id ="sub-heading">This is a test</h2>
+//<p class = "color">My favorite color is Orange.</p>
+//<button id = "submit">submit button</button>
+
+document.querySelector("#sub-heading");
+console.log(document.querySelector("sub-heading"));
+
+document.querySelector(".color");
+console.log(document.querySelector(".color"));
+
+document.querySelector("#submit");
+console.log(document.querySelector("#submit"));
